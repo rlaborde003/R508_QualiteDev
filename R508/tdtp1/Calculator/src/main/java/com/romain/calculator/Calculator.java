502132a22760bd4b5fc08825e3a664fefc93c035
Romain Laborde <r.laborde@etud.univ-pau.fr>
2024-09-25 16:52:57 +0200

    test Set

--- R508/tdtp1/Calculator/src/main/java/com/romain/calculator/Calculator.java
+++ R508/tdtp1/Calculator/src/main/java/com/romain/calculator/Calculator.java
@@ -1,5 +1,8 @@
 package com.romain.calculator;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Calculator {
 
     public static int add(int opG, int opD) {
@@ -13,5 +16,25 @@ public class Calculator {
         else {
             throw new RuntimeException("Division par 0 impossible");
         }
+    }
+
+    public static Set<Integer>ensembleChiffres(int pNombre) {
+        String nbStr = Integer.toString(pNombre).replace("-", "").replace("+","");
+        Set<Integer> nb = new HashSet<Integer>();
+        StringBuilder newStr = new StringBuilder();
+        char[] charArray = nbStr.toCharArray();
+
+        for (char c : charArray)
+            newStr.
+            if (!newStr.contains(Character.toString(c))) {
+                newStr.append(Character.toString(c));
+            }
+
+        char[] char2Array = newStr.toString().toCharArray();
+        for (char c: char2Array) {
+            boolean add = nb.add(Integer.parseInt(Character.toString(c)));
+        }
+
+        return nb;
     }
 }
