12fffc9ff346f02aedacb89929431fcf2ac502e8
Romain Laborde <r.laborde@etud.univ-pau.fr>
2024-09-25 16:56:25 +0200

    test modif fichier

--- R508/tdtp1/Calculator/src/main/java/com/romain/calculator/Calculator.java
+++ R508/tdtp1/Calculator/src/main/java/com/romain/calculator/Calculator.java
@@ -1,40 +1,26 @@
-package com.romain.calculator;
+package calculator;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class Calculator {
 
-    public static int add(int opG, int opD) {
-        return opG + opD;
-    }
+    public static int add(int opG, int opD) {return opG + opD;}
+    public static int divide(int opG, int opD) {return opG/opD;}
 
-    public static int divide(int opG, int opD) {
-        if (opD != 0) {
-            return opG / opD;
+    public static Set<Integer> ensembleChiffres(int pNombre) {
+        Set<Integer> entiers = new HashSet<Integer>();
+        String chaineDuNombre = String.valueOf(pNombre);
+
+        if ((chaineDuNombre.charAt(0) != '-') && (chaineDuNombre.charAt(0) != '+')) {
+            entiers.add(Integer.parseInt(chaineDuNombre, 0, 0 + 1, 10));
         }
-        else {
-            throw new RuntimeException("Division par 0 impossible");
-        }
-    }
 
-    public static Set<Integer>ensembleChiffres(int pNombre) {
-        String nbStr = Integer.toString(pNombre).replace("-", "").replace("+","");
-        Set<Integer> nb = new HashSet<Integer>();
-        StringBuilder newStr = new StringBuilder();
-        char[] charArray = nbStr.toCharArray();
-
-        for (char c : charArray)
-            newStr.
-            if (!newStr.contains(Character.toString(c))) {
-                newStr.append(Character.toString(c));
+        for (int i = 1; i < chaineDuNombre.length(); i++) {
+            {
+                entiers.add(Integer.parseInt(chaineDuNombre, i, i + 1, 10));
             }
-
-        char[] char2Array = newStr.toString().toCharArray();
-        for (char c: char2Array) {
-            boolean add = nb.add(Integer.parseInt(Character.toString(c)));
         }
-
-        return nb;
+        return entiers;
     }
 }
--- R508/tdtp1/Calculator/src/test/java/com/romain/calculator/CalculatorTest.java
+++ R508/tdtp1/Calculator/src/test/java/com/romain/calculator/CalculatorTest.java
@@ -1,68 +1,117 @@
-package com.romain.calculator;
+package calculator;
 
-import org.assertj.core.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import java.util.Set;
+
 
 class CalculatorTest {
 
-    // given
-    int opG;
-    int opD;
+        /*@BeforeEach void setUp() {
+            calculator = new Calculator();
+        }
 
-    @BeforeEach
-    void set_up() {
-        // given
-        opG = 10;
-        opD = 2;
-    }
+        @AfterEach void tearDown() {
+            calculator = null;
+        }*/
 
-    @ParameterizedTest(name = "{0} + {1} = {2}")
-    @CsvSource({
-            "0, 1, 1",
-            "1, 2, 3",
-            "-2, 2, 0",
-            "0, 0, 0",
-            "-1, -2, -3"
-    })
+        @Test
+        void devrait_obtenir_le_resultat_de_laddition() {
+            //GIVEN
+            int opG = 2;
+            int opD = 3;
 
-    void add_devrait_renvoyer_addition(int opG, int opD, int expectResult) {
-        // when
-        var result = Calculator.add(opG, opD);
+            //WHEN
+            int resultat = Calculator.add(opG, opD);
 
-        //then
-        assertThat(result).isEqualTo(expectResult);
-    }
+            //THEN
+            assertThat(resultat).isEqualTo(5);
+        }
 
-    @Test
-    void divide_devrait_renvoyer_erreur() {
-        // given
-        int opD = 0;
+        @ParameterizedTest(name = "{0} + {1} = {2}")
+        @CsvSource({
+                "0,    1,   1",
+                "1,    2,   3",
+                "-2,   2,   0",
+                "0,    0,   0",
+                "-1,   -2,  -3"
+        })
 
-        // when
-        Throwable err = Assertions.catchThrowable(() -> Calculator.divide(opG, opD));
+        void add_parametre_devrait_calculer_la_somme_de_deux_int(int first, int second, int expectedResult)  {
+            // GIVEN
 
-        //then
-        assertThat(err).hasMessage("Division par 0 impossible");
-    }
+            // WHEN
+            int somme = Calculator.add(first, second);
+            // THEN  -- SI on ne s'occupe pas de l'exception levée
 
-    @Test
-    void divide_devrait_renvoyer_diviseur() {
-        // when
-        var result = Calculator.divide(opG, opD);
+            //assertEquals(expectedResult, calculatorEnTest.add(first, second),
+            //		() -> first + " + " + second + " should equal " + expectedResult);	// JUnit
+            assertThat(somme).isEqualTo(expectedResult);
+        }
 
-        //then
-        assertThat(result).isEqualTo(opG/opD);
-    }
+        @Test
+        void divide_devrait_retourner_quotient_nul_quand_diviseur_plus_grand_que_dividende()
+        {
+            // GIVEN
 
-    @AfterEach
-    void tear_down() {
-        System.out.println("test ok");
-    }
+            // WHEN
+            int quotient = Calculator.divide(1,2);
 
+            //THEN
+            assertThat(quotient).isEqualTo(0);
+        }
+
+        @Test
+        void divide_devrait_retourner_quotient_non_nul_quand_diviseur_plus_petit_que_dividende()
+        {
+            // GIVEN
+
+            // WHEN
+            int quotient = Calculator.divide(7,2);
+
+            //THEN
+            assertThat(quotient).isEqualTo(3);
+        }
+
+        @Test
+        void digitsSet_devrait_retourner_les_chiffres_d_un_entier_positif() {
+            //GIVEN
+            int entierPositif = 97689;
+
+            //WHEN
+            Set<Integer> ensembleChiffres = Calculator.ensembleChiffres(entierPositif);
+
+            //THEN
+            assertThat(ensembleChiffres).containsExactlyInAnyOrder(9, 6, 8, 7);
+        }
+
+        @Test
+        void digitsSet_devrait_retourner_les_chiffres_d_un_entier_negatif() {
+            //GIVEN
+            int entierNegatif = -1;
+
+            //WHEN
+            Set<Integer> ensembleChiffres = Calculator.ensembleChiffres(entierNegatif);
+
+            //THEN
+            assertThat(ensembleChiffres).containsExactlyInAnyOrder(1);
+        }
+        @Test
+        void digitsSet_devrait_retourner_le_chiffre_0_d_un_entier_nul() {
+            //GIVEN
+            int entierNul = 0000;
+
+            //WHEN
+            Set<Integer> ensembleChiffres = Calculator.ensembleChiffres(entierNul);
+
+            //THEN
+            assertThat(ensembleChiffres).containsExactlyInAnyOrder(0);
+        }
 }
